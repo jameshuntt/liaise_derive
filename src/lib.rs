9107e7439028074bf818ecffc1660abaa21fa06a
James Local <local@james.com>
2026-02-19 22:45:39 -0500

    new derive

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 // use quote::quote;
 // use syn::{parse_macro_input, DeriveInput, Data};
 
-fn parse_error_prefix(attrs: &[syn::Attribute]) -> syn::Result<String> {
+fn parse_error_prefixa(attrs: &[syn::Attribute]) -> syn::Result<String> {
     for attr in attrs {
         if !attr.path().is_ident("error_prefix") {
             continue;
@@ -51,7 +51,7 @@ struct LiaiseVariantAttr {
     source: bool,
 }
 
-fn parse_liaise_variant_attr(attrs: &[syn::Attribute]) -> syn::Result<LiaiseVariantAttr> {
+fn parse_liaise_variant_attra(attrs: &[syn::Attribute]) -> syn::Result<LiaiseVariantAttr> {
     let mut out = LiaiseVariantAttr::default();
 
     for attr in attrs {
@@ -241,13 +241,299 @@ pub fn derive_register_errors(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
-#[proc_macro_derive(LiaiseCodes, attributes(error_prefix, liaise))]
+// #[proc_macro_derive(LiaiseCodes, attributes(error_prefix, liaise))]
+// pub fn derive_liaise_codes(input: TokenStream) -> TokenStream {
+//     let input = parse_macro_input!(input as DeriveInput);
+//     let name = &input.ident;
+// 
+//     let prefix = match parse_error_prefix(&input.attrs) {
+//         Ok(p) => p,
+//         Err(e) => return e.to_compile_error().into(),
+//     };
+// 
+//     let variants = match &input.data {
+//         Data::Enum(d) => &d.variants,
+//         _ => {
+//             return syn::Error::new_spanned(name, "LiaiseCodes can only be derived for enums")
+//                 .to_compile_error()
+//                 .into();
+//         }
+//     };
+// 
+//     struct Info {
+//         ident: syn::Ident,
+//         fields: Fields,
+//         code: u16,
+//         msg: syn::LitStr,
+//         span: proc_macro2::Span,
+//     }
+// 
+//     let mut infos = Vec::<Info>::new();
+// 
+//     for v in variants {
+//         let meta = match parse_liaise_meta(&v.attrs) {
+//             Ok(m) => m,
+//             Err(e) => return e.to_compile_error().into(),
+//         };
+// 
+//         let code = match meta.code {
+//             Some(c) => c,
+//             None => {
+//                 return syn::Error::new(v.span(), "missing #[liaise(code = ...)] on variant")
+//                     .to_compile_error()
+//                     .into();
+//             }
+//         };
+// 
+//         let msg = match meta.msg {
+//             Some(m) => m,
+//             None => {
+//                 return syn::Error::new(v.span(), "missing #[liaise(msg = \"...\")] on variant")
+//                     .to_compile_error()
+//                     .into();
+//             }
+//         };
+// 
+//         infos.push(Info {
+//             ident: v.ident.clone(),
+//             fields: v.fields.clone(),
+//             code,
+//             msg,
+//             span: v.span(),
+//         });
+//     }
+// 
+//     // ALL_CODES
+//     let all_codes: Vec<u16> = infos.iter().map(|i| i.code).collect();
+// 
+//     // match arms
+//     let code_arms = infos.iter().map(|i| {
+//         let vi = &i.ident;
+//         let code = i.code;
+//         match &i.fields {
+//             Fields::Unit => quote! { Self::#vi => #code, },
+//             Fields::Unnamed(_) => quote! { Self::#vi(..) => #code, },
+//             Fields::Named(_) => quote! { Self::#vi { .. } => #code, },
+//         }
+//     });
+// 
+//     let msg_arms = infos.iter().map(|i| {
+//         let vi = &i.ident;
+//         let msg = &i.msg;
+//         match &i.fields {
+//             Fields::Unit => quote! { Self::#vi => ::alloc::format!(#msg), },
+//             // tuple variants can't bind named fields; keep it simple
+//             Fields::Unnamed(_) => quote! { Self::#vi(..) => ::alloc::format!(#msg), },
+//             Fields::Named(named) => {
+//                 let binds = named.named.iter().map(|f| f.ident.as_ref().unwrap());
+//                 quote! { Self::#vi { #(#binds),* } => ::alloc::format!(#msg), }
+//             }
+//         }
+//     });
+// 
+//     let expanded = quote! {
+//         impl ::liaise::ErrorRegistry for #name {
+//             const PREFIX: &'static str = #prefix;
+//             const ALL_CODES: &'static [u16] = &[ #(#all_codes),* ];
+//         }
+// 
+//         const _: () = <#name as ::liaise::ErrorRegistry>::VALIDATE;
+// 
+//         impl ::liaise::Liaise for #name {
+//             fn code_id(&self) -> u16 {
+//                 match self {
+//                     #(#code_arms)*
+//                 }
+//             }
+// 
+//             fn message(&self) -> ::alloc::string::String {
+//                 match self {
+//                     #(#msg_arms)*
+//                 }
+//             }
+//         }
+// 
+//         impl ::core::fmt::Display for #name {
+//             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+//                 // Liaise::render takes self by value; your traits require Copy, so this is fine.
+//                 write!(f, "{}", (*self).render())
+//             }
+//         }
+//     };
+// 
+//     TokenStream::from(expanded)
+// }
+
+
+
+use syn::{Attribute
+};
+
+fn parse_error_prefix(attrs: &[Attribute]) -> syn::Result<Option<String>> {
+    for attr in attrs {
+        if !attr.path().is_ident("error_prefix") {
+            continue;
+        }
+
+        // #[error_prefix("ABUT")]
+        if let syn::Meta::List(list) = &attr.meta {
+            let lit: syn::LitStr = syn::parse2(list.tokens.clone())?;
+            return Ok(Some(lit.value()));
+        }
+
+        // #[error_prefix = "ABUT"]
+        if let syn::Meta::NameValue(nv) = &attr.meta {
+            if let syn::Expr::Lit(expr_lit) = &nv.value {
+                if let syn::Lit::Str(lit) = &expr_lit.lit {
+                    return Ok(Some(lit.value()));
+                }
+            }
+            return Err(syn::Error::new_spanned(nv, "expected #[error_prefix = \"ABUT\"]"));
+        }
+
+        return Err(syn::Error::new_spanned(
+            attr,
+            "expected #[error_prefix(\"ABUT\")] or #[error_prefix = \"ABUT\"]",
+        ));
+    }
+
+    Ok(None)
+}
+
+fn parse_liaise_prefix(attrs: &[Attribute]) -> syn::Result<Option<String>> {
+    for attr in attrs {
+        if !attr.path().is_ident("liaise") {
+            continue;
+        }
+
+        // #[liaise(prefix = "ABUT")]
+        let items: Punctuated<Meta, Token![,]> = attr.parse_args_with(Punctuated::parse_terminated)?;
+        for meta in items {
+            match meta {
+                Meta::NameValue(MetaNameValue { path, value, .. }) if path.is_ident("prefix") => {
+                    let lit = match value {
+                        Expr::Lit(el) => el.lit,
+                        other => {
+                            return Err(syn::Error::new_spanned(
+                                other,
+                                "liaise(prefix = \"ABUT\") expected",
+                            ));
+                        }
+                    };
+                    let s = match lit {
+                        Lit::Str(ls) => ls.value(),
+                        other => {
+                            return Err(syn::Error::new_spanned(
+                                other,
+                                "liaise(prefix = \"ABUT\") expected",
+                            ));
+                        }
+                    };
+                    return Ok(Some(s));
+                }
+                other => {
+                    // On the enum itself we only allow prefix=...
+                    return Err(syn::Error::new_spanned(
+                        other,
+                        "on the enum, #[liaise(...)] only supports prefix = \"...\"",
+                    ));
+                }
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+
+
+fn parse_liaise_variant_attr(attrs: &[Attribute]) -> syn::Result<LiaiseVariantAttr> {
+    let mut out = LiaiseVariantAttr::default();
+
+    for attr in attrs {
+        if !attr.path().is_ident("liaise") {
+            continue;
+        }
+
+        // #[liaise(code = 1, msg = "...", source)]
+        let items: Punctuated<Meta, Token![,]> = attr.parse_args_with(Punctuated::parse_terminated)?;
+
+        for meta in items {
+            match meta {
+                Meta::NameValue(MetaNameValue { path, value, .. }) if path.is_ident("code") => {
+                    let lit = match value {
+                        Expr::Lit(el) => el.lit,
+                        other => {
+                            return Err(syn::Error::new_spanned(
+                                other,
+                                "liaise(code = <u16 integer literal>) expected",
+                            ));
+                        }
+                    };
+                    let n = match lit {
+                        Lit::Int(li) => li.base10_parse::<u16>()?,
+                        other => {
+                            return Err(syn::Error::new_spanned(
+                                other,
+                                "liaise(code = <u16 integer literal>) expected",
+                            ));
+                        }
+                    };
+                    out.code = Some(n);
+                }
+
+                Meta::NameValue(MetaNameValue { path, value, .. })
+                    if path.is_ident("msg") || path.is_ident("message") =>
+                {
+                    let lit = match value {
+                        Expr::Lit(el) => el.lit,
+                        other => {
+                            return Err(syn::Error::new_spanned(other, "liaise(msg = \"...\") expected"));
+                        }
+                    };
+                    let s = match lit {
+                        Lit::Str(ls) => ls,
+                        other => return Err(syn::Error::new_spanned(other, "liaise(msg = \"...\") expected")),
+                    };
+                    out.msg = Some(s);
+                }
+
+                Meta::Path(p) if p.is_ident("source") => {
+                    out.source = true;
+                }
+
+                // IMPORTANT: ignore prefix= on variants if someone accidentally copies it
+                Meta::NameValue(MetaNameValue { path, .. }) if path.is_ident("prefix") => {}
+
+                other => {
+                    return Err(syn::Error::new_spanned(
+                        other,
+                        "unsupported #[liaise(...)] item; expected code=, msg=, and/or source",
+                    ));
+                }
+            }
+        }
+    }
+
+    Ok(out)
+}
+
+#[proc_macro_derive(LiaiseCodes, attributes(liaise, error_prefix))]
 pub fn derive_liaise_codes(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
-    let prefix = match parse_error_prefix(&input.attrs) {
-        Ok(p) => p,
+    // prefix resolution order:
+    // 1) #[liaise(prefix="...")]
+    // 2) #[error_prefix("...")] (your old form)
+    // 3) "ERR"
+    let prefix = match parse_liaise_prefix(&input.attrs) {
+        Ok(Some(p)) => p,
+        Ok(None) => match parse_error_prefix(&input.attrs) {
+            Ok(Some(p)) => p,
+            Ok(None) => "ERR".to_string(),
+            Err(e) => return e.to_compile_error().into(),
+        },
         Err(e) => return e.to_compile_error().into(),
     };
 
@@ -265,13 +551,15 @@ pub fn derive_liaise_codes(input: TokenStream) -> TokenStream {
         fields: Fields,
         code: u16,
         msg: syn::LitStr,
+        source: bool,
+        source_ty: Option<syn::Type>,
         span: proc_macro2::Span,
     }
 
     let mut infos = Vec::<Info>::new();
 
     for v in variants {
-        let meta = match parse_liaise_meta(&v.attrs) {
+        let meta = match parse_liaise_variant_attr(&v.attrs) {
             Ok(m) => m,
             Err(e) => return e.to_compile_error().into(),
         };
@@ -294,11 +582,31 @@ pub fn derive_liaise_codes(input: TokenStream) -> TokenStream {
             }
         };
 
+        let mut source_ty: Option<syn::Type> = None;
+
+        if meta.source {
+            match &v.fields {
+                Fields::Unnamed(u) if u.unnamed.len() == 1 => {
+                    source_ty = Some(u.unnamed.first().unwrap().ty.clone());
+                }
+                _ => {
+                    return syn::Error::new(
+                        v.span(),
+                        "`source` is only valid on single-field tuple variants like `Io(std::io::Error)`",
+                    )
+                    .to_compile_error()
+                    .into();
+                }
+            }
+        }
+
         infos.push(Info {
             ident: v.ident.clone(),
             fields: v.fields.clone(),
             code,
             msg,
+            source: meta.source,
+            source_ty,
             span: v.span(),
         });
     }
@@ -322,13 +630,33 @@ pub fn derive_liaise_codes(input: TokenStream) -> TokenStream {
         let msg = &i.msg;
         match &i.fields {
             Fields::Unit => quote! { Self::#vi => ::alloc::format!(#msg), },
-            // tuple variants can't bind named fields; keep it simple
             Fields::Unnamed(_) => quote! { Self::#vi(..) => ::alloc::format!(#msg), },
             Fields::Named(named) => {
                 let binds = named.named.iter().map(|f| f.ident.as_ref().unwrap());
                 quote! { Self::#vi { #(#binds),* } => ::alloc::format!(#msg), }
             }
         }
+    });
+
+    let source_arms = infos.iter().filter_map(|i| {
+        if !i.source {
+            return None;
+        }
+        let vi = &i.ident;
+        // match ergonomics will bind `src` as a reference because `self` is `&Self`
+        Some(quote! { Self::#vi(src) => Some(src), })
+    });
+
+    let from_impls = infos.iter().filter_map(|i| {
+        let vi = &i.ident;
+        let ty = i.source_ty.as_ref()?;
+        Some(quote! {
+            impl From<#ty> for #name {
+                fn from(src: #ty) -> Self {
+                    Self::#vi(src)
+                }
+            }
+        })
     });
 
     let expanded = quote! {
@@ -355,10 +683,20 @@ pub fn derive_liaise_codes(input: TokenStream) -> TokenStream {
 
         impl ::core::fmt::Display for #name {
             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-                // Liaise::render takes self by value; your traits require Copy, so this is fine.
-                write!(f, "{}", (*self).render())
+                write!(f, "{}", self.render())
             }
         }
+
+        impl ::core::error::Error for #name {
+            fn source(&self) -> Option<&(dyn ::core::error::Error + 'static)> {
+                match self {
+                    #(#source_arms)*
+                    _ => None,
+                }
+            }
+        }
+
+        #(#from_impls)*
     };
 
     TokenStream::from(expanded)
